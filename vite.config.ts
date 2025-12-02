d1ade77122587ab2c9cf4bbb577588d9363902a2
ygfhchatse <ygfchatse@gmail.com>
2025-12-02 23:06:06 +0530

    Fix vite base configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/YashChatse.github.io/',
+  base: '/',
   plugins: [
     react({
       babel: {
